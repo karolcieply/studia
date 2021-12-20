a2ab282d9d60f9fd043c61ccb73beb2273996465
Karol Cieply <karolcieply00@gmail.com>
2021-12-20 17:09:16 +0100

    finished all searching algorithms

---
+++ semestrIII/algorytmy/kR.py
@@ -0,0 +1,28 @@
+def kR(text, pattern, hPattern, f, q):
+    for i in range(len(text)):
+        temp = ''
+        for j in range(i, len(pattern)+i, 1):
+            if j < len(text):
+                temp += text[j]
+        hTemp = h(temp, f, q)
+        if hTemp == hPattern:
+            return i
+
+def h(pattern, f, q):
+    result = 0
+    for i in range(len(pattern)):
+        result += f[pattern[i]]*len(pattern)**(len(pattern)-1-i)
+    return result % q
+
+
+text = "abacdcbdacd"
+pattern = "acdc"
+i = 0
+q = 23
+f = {}
+for letter in sorted(text):
+    if letter not in f:
+        f[letter] = i
+        i += 1
+hPattern = h(pattern, f, q)
+print(kR(text, pattern, hPattern, f, q))
--- semestrIII/algorytmy/wyszukiwania/bm.py
+++ semestrIII/algorytmy/wyszukiwania/bm.py
@@ -1,6 +1,20 @@
-lastIndexes = {"a": 3,
-               "b": 4,
-               "c": 5,
-               "d": 2}
 def bM(text, pattern):
-    for 
+    last = {}
+    for i, v in enumerate(pattern):
+        last[v] = i
+    i = len(pattern) - 1
+    j = len(pattern) - 1
+    while i < len(text):
+        if text[i] == pattern[j]:
+            if j == 0:
+                return i
+            else:
+                i -= 1
+                j -= 1
+        else:
+            i = i + len(pattern) - min(j, 1 + last.get(text[i], 0))
+            j = len(pattern) - 1
+    return -1
+
+
+print(bM("csfbadxbcabdzabdabc", "abdabc"))
